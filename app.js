a35ebbdfdcbfe964170c4c45280edd4df6a574d4
rob587 <robertocamm02@yahoo.com>
2025-08-01 16:13:45 +0200

    aggiunte routes

--- app.js
+++ app.js
@@ -20,6 +20,30 @@ app.get('/Torte/:id', (req, res)=>{
     res.send(`dettaglio della torta con id: ${req.params.id}`)
 })
 
+// create
+
+app.post('/Torte', (req,res)=>{
+    res.send('creazione della torta')
+})
+
+// update
+
+app.put('/Torte/:id', (req,res)=>{
+    res.send(`modifica totale della torta con id: ${req.params.id} `)
+})
+
+// patch
+
+app.patch('/Torte/:id',(req,res)=>{
+    res.send(`modifica parziale della torta con id ${req.params.id}`)
+})
+
+// delete
+
+app.delete('/Torte/:id', (req,res)=>{
+    res.send(`Cancella torta con id ${req.params.id}`)
+})
+
 
 app.listen(port, ()=>{
     console.log('server della mia pasticceria')
