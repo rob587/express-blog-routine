5b5c44491ff2d99e66c56940723132528789a8fe
rob587 <robertocamm02@yahoo.com>
2025-08-01 16:07:12 +0200

    [fix]

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ app.get('/', (req, res)=>{
     res.send('La mia Pasticceria')
 })
 
+// index
+app.get('/Torte', (req, res)=>{
+    res.send('elenco delle Torte')
+})
+
+// show
+app.get('/Torte/:id', (req, res)=>{
+    res.send(`dettaglio della torta con id: ${req.params.id}`)
+})
+
 
 app.listen(port, ()=>{
     console.log('server della mia pasticceria')
